1b5eedea90ce321ba0bed78e58835b4203f0c98c
MINESORT <ilyamironenko1314@gmail.com>
2024-03-06 18:04:39 +0300

    Initial commit

--- app/src/main/java/com/practicum/playlistmakerhomework1/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmakerhomework1/MainActivity.kt
@@ -7,13 +7,15 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.Switch
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val switch6 = findViewById<Switch>(R.id.switch6)
         val settingsButton = findViewById<Button>(R.id.buttonMain3)
         val libraryButton = findViewById<Button>(R.id.buttonMain2)
         val searchButton = findViewById<Button>(R.id.buttonMain1)
@@ -23,8 +25,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(displayIntent)
         }
 
-        val imageClickListener: View.OnClickListener = View.OnClickListener { val displayIntent = Intent(this, SearchActivity::class.java)
-            startActivity(displayIntent) }
+        val imageClickListener: View.OnClickListener = View.OnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+        }
         searchButton.setOnClickListener(imageClickListener)
 
 
@@ -32,8 +36,6 @@ class MainActivity : AppCompatActivity() {
             val displayIntent = Intent(this, MediaLibrary::class.java)
             startActivity(displayIntent)
         }
-
-
 
     }
 }
